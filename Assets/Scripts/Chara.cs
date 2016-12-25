99f3706d2af3ee228bb05e79fe5b66661d05ecd1
mgotoks <mgotoks@gmail.com>
2016-12-25 21:05:00 +0900

    first commit

---
+++ Assets/Scripts/Chara.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chara : MonoBehaviour {
+
+	Rigidbody2D rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		
+	}
+
+	public void move(float dx){
+		Vector2 vec = rigidBody.velocity;
+		vec.x = dx;
+		rigidBody.velocity = vec;
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+
+	Chara chara;
+
+	void Start(){
+		GameObject obj = GameObject.Find ("Chara") as GameObject;
+		chara = obj.GetComponent<Chara> ();
+	}
+		
+	//右ボタンを押した時に呼び出す
+	//RightButton->Event Trigger(Script)->Pointer Down
+	public void onRightButtonDown(){
+		Debug.Log("right button clicked");
+		chara.move (2.0f);
+	}
+
+	//右ボタンを離した時に呼び出す
+	//RightButton->Event Trigger(Script)->Pointer Up
+	public void onRightButtonUp(){
+		Debug.Log ("right button up");
+		chara.move (0);
+	}
+
+	//左ボタンを押した時に呼び出す
+	//LeftButton->Event Trigger(Script)->Pointer Down
+	public void onLeftButtonDown(){
+		Debug.Log ("left button clicked");
+		chara.move (-2.0f);
+	}
+
+	//左ボタンを離した時に呼び出す
+	//LeftButton->Event Trigger(Script)->Pointer Up
+	public void onLeftButtonUp(){
+		Debug.Log ("left button up");
+		chara.move (0);
+	}
+}
